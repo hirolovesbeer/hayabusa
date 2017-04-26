d86394b0f0dafb193b0ca36c8395e19f1649d87d
hirolovesbeer <hirolovesbeer@gmail.com>
2017-04-26 16:59:04 +0900

    add dir path creation

--- store_engine.py
+++ store_engine.py
@@ -1,18 +1,26 @@
 import os.path
 import sqlite3
 
-log_file = '/var/log/message.1'
-db_file = 'test.db'
+import datetime
+from distutils.dir_util import mkpath
 
-if not os.path.exists(db_file):
+log_file = '/var/log/message.1'
+
+now = datetime.datetime.now()
+dir_path = '%d/%02d/%02d/%02d ' % (now.year, now.month, now.day, now.hour)
+db_file = '%.db' % now.minute
+db_path = dirpath + '/' + db_file
+
+mpkath(dir_path)
+
+if not os.path.exists(db_path):
     conn = sqlite3.connect(db_file)
     conn.execute("CREATE VIRTUAL TABLE SYSLOG USING FTS3(LOGS)");
     conn.close()
 
-conn = sqlite3.connect(db_file)
+conn = sqlite3.connect(db_path)
 conn.execute("PRAGMA SYNCHRONOUS = OFF");
 conn.execute("PRAGMA JOURNAL_MODE = MEMORY");
-
 
 with open(log_file) as fh:
     lines = [[line] for line in fh]

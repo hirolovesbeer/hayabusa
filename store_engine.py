d6ecea2ac1d624f4a3fabbbfb5ee265cf48f4a02
Hiroshi ABE <hirolovesbeer@gmail.com>
2018-10-15 15:18:35 +0900

    fix db file format

--- store_engine.py
+++ store_engine.py
@@ -12,18 +12,16 @@ CONFIG   = '/path/to/config.ini'
 def main():
     now = datetime.datetime.now()
     dir_path = '%d/%02d/%02d/%02d' % (now.year, now.month, now.day, now.hour)
-    db_file = '%d.db' % (now.minute)
+    db_file = '%02d.db' % (now.minute)
     db_path = '%s/%s/%s' % (BASE_DIR, dir_path, db_file)
 
     dir_path = '%s/%s' %(BASE_DIR, dir_path)
     mkpath(dir_path)
 
-    if not os.path.exists(db_path):
-        conn = sqlite3.connect(db_path)
-        conn.execute("CREATE VIRTUAL TABLE SYSLOG USING FTS3(LOGS)");
-        conn.close()
-
+    if os.path.exists(db_path):
+        os.remove(db_path)
     conn = sqlite3.connect(db_path)
+    conn.execute("CREATE VIRTUAL TABLE SYSLOG USING FTS3(LOGS)");
     conn.execute("PRAGMA SYNCHRONOUS = OFF");
     conn.execute("PRAGMA JOURNAL_MODE = MEMORY");
 
